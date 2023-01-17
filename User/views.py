b4a9d944b6b75df9dc9a5bebe73e7803544bd281
Lockco <93267401+Lockco@users.noreply.github.com>
2023-01-17 08:47:32 +0100

    Update views.py

--- User/views.py
+++ User/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from User.models import User
 
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(User, id=user_id)
+    request.user.followers.add(user_to_follow)
+    return redirect('user_profile', user_id=user_id)
 
-# Create your views here.
-def index(request):
-	return render(request, "User/index.html")
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(User, id=user_id)
+    request.user.followers.remove(user_to_unfollow)
+    return redirect('user_profile', user_id=user_id)
