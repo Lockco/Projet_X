323567d3f49bb63567011ed1099eeefe510c6baf
Lockco <93267401+Lockco@users.noreply.github.com>
2023-01-27 15:56:16 +0100

    Add files via upload

---
+++ src/followers/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from .models import UserFollows
+
+# Enregistrez UserFollows dans l'interface d'administration
+admin.site.register(UserFollows)
+
+# Enregistrez User dans l'interface d'administration
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
